55337bca7626c1a88c77e89f7d82db2849b22ffb
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-07 21:51:44 +0530

    Added New concept in my list

---
+++ 01-variables.sh
@@ -0,0 +1,45 @@
+# If we assign a name to set of data it is called as a variable.
+# Syntax : var_name=data
+
+x=10
+
+
+# LHS - RHS
+#LHS
+ # Variable name can comprise of a-z, A-Z, 0-9, _
+ # Variable should not start with a num , x1 is good to use, 1x is not to use
+
+#RHS
+ # Data is shell is treated as string by default.
+ # By default you don't have any data types.
+
+x=10
+y=abc
+z=xyz123
+
+# If we have any special characters in the RHS then we can provide that in quotes.
+x1="abc 123"
+
+
+
+# Access the variable
+ # Syntax: $var_name, or ${var_name}
+
+file=1.txt
+touch $file
+cp $file 2.txt
+
+# Dynamic declaration of variables
+
+# Command Substitution
+# Syntax: var=$(command)
+
+list_of_files=$(ls)
+
+echo $list_of_files
+
+# Arithmetic Substitution
+# Syntax: var=$((expression))
+add=$((2+3))
+
+echo $add
